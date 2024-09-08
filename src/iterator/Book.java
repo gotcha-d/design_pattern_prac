6796c506df06862ee21150e0cbeb66162285ff5a
gotcha-d <gotcha.privatemail@gmail.com>
2024-09-08 11:35:06 +0900

    Iteratorパターンのサンプル作成

---
+++ src/iterator/Book.java
@@ -0,0 +1,17 @@
+package iterator;
+
+/**
+ * Book
+ */
+public class Book {
+
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
