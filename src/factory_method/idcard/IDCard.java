27a92f557db94cd47e73bd6ac1f4bb7e513615ac
gotcha-d <gotcha.privatemail@gmail.com>
2024-09-14 14:17:00 +0900

    Factory Method 練習問題完了

--- src/factory_method/idcard/IDCard.java
+++ src/factory_method/idcard/IDCard.java
@@ -5,15 +5,17 @@ import factory_method.framework.Product;
 public class IDCard extends Product {
     
     private String owner;
+    private int id;
 
-    IDCard(String owner) {
-        System.out.println(owner + "のカードを作ります");
+    IDCard(String owner, int id) {
+        System.out.println(owner + "のカードを作ります(ID: " + id + ")");
         this.owner = owner;
+        this.id = id;
     }
 
     @Override
     public void use() {
-        System.out.println(this + "を使います");
+        System.out.println(this + "を使います(ID: " + id + ")");
     }
 
     @Override
--- src/factory_method/idcard/IDCardFactory.java
+++ src/factory_method/idcard/IDCardFactory.java
@@ -1,13 +1,20 @@
 package factory_method.idcard;
 
+import java.util.HashMap;
+
 import factory_method.framework.Factory;
 import factory_method.framework.Product;
 
 public class IDCardFactory extends Factory {
     
+    private HashMap<Integer, String> table = new HashMap();
+    private int id = 0;
+
     @Override
     protected Product createProduct(String owner) {
-        return new IDCard(owner);
+        id++;
+        table.put(id, owner);
+        return new IDCard(owner, id);
     }
 
     @Override
